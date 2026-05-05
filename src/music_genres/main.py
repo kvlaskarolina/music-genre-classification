ad74685faf6e36115ea8083347d8f59725092ef4
Łukasz Drukała <masflam@masflam.com>
2026-05-05 18:41:18 +0200

    apple ai shit support?

--- src/music_genres/main.py
+++ src/music_genres/main.py
@@ -26,7 +26,12 @@ def build_transform() -> torch.nn.Module:
 
 
 def main() -> None:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     print(f"Device: {device}")
 
     print("Downloading dataset...")
